29a2c9dfce8a710e2d4f545fe6d5f26c79f2d520
Durotan <50570415+JanFojtik21@users.noreply.github.com>
2024-02-06 15:20:10 +0100

    Nemoge rozjet

--- ChatServer.java
+++ ChatServer.java
@@ -1,12 +1,17 @@
 import java.io.*;
+import java.lang.reflect.Executable;
 import java.net.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ChatServer {
 
     private int port;
+    private ExecutorService executorService;    
 
     public ChatServer(int port){
         this.port = port;
+        this.executorService = Executors.newFixedThreadPool(10);
     }
 
     public void startServer() throws ServerException {
@@ -16,10 +21,41 @@ public class ChatServer {
             while (true) {
                 Socket socket = serverSocket.accept();
                 System.out.println("New client connected");
+                Runnable clientHandler = new ClientHandler(socket);
+                executorService.execute(clientHandler);
             }
         } catch (IOException ex) {
             throw new ServerException("Error in the server: " + ex.getMessage(), ex);
         }
+    }
+
+    public class ClientHandler implements Runnable{
+        private Socket socket;
+
+        public ClientHandler(Socket socket){
+            this.socket = socket;
+        }
+        @Override
+        public void run(){
+            try {
+                InputStream input = socket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                OutputStream output = socket.getOutputStream();
+                PrintWriter writer = new PrintWriter(output, true);
+
+                String message;
+                do {
+                    message = reader.readLine();
+                    System.out.println("Received message: " + message);
+                    writer.println("Server received the message: " + message);
+                } while (!message.equals("exit"));
+                socket.close();
+            } catch (IOException ex) {
+                System.out.println("Error in the server: " + ex.getMessage());
+                ex.printStackTrace();
+            }
+        }
+
     }
 
     public static void main (String [] args){
